89041902ed532a1e60e4c99d799ca2dae2485994
Bucky <bucky@buckydemacbook-pro.local>
2016-11-17 18:21:59 +0800

    Initial Commit

---
+++ customer/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  customer
+//
+//  Created by Bucky on 2016/11/17.
+//  Copyright © 2016年 Bucky. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
